65a9e8c9d6705972cd079d83c7ea8bc69b089985
6574114 <j.f.wiegner@uu.nl>
2024-06-24 15:28:43 +0200

    network dc on updated

---
+++ mes_north_sea/postprocessing/process_summary.py
@@ -0,0 +1,155 @@
+import pandas as pd
+from pathlib import  Path
+import h5py
+from src.result_management.read_results import *
+
+dir = Path("//ad.geo.uu.nl/Users/StaffUsers/6574114/EhubResults/MES "
+            "NorthSea/baseline_demand_v6/Summary_temp.xlsx")
+dir_processed = Path("//ad.geo.uu.nl/Users/StaffUsers/6574114/EhubResults/MES "
+            "NorthSea/baseline_demand_v6/Summary_processed.xlsx")
+
+h2_emissions = 29478397.12
+scenarios = {
+               '20240308120552_Baseline_costs': ['Baseline', 'Baseline'],
+              'Battery_on': ['Storage', 'onshore only'],
+              'Battery_off': ['Storage', 'offshore only'],
+              'Battery_all': ['Storage', 'all'],
+              'Battery_all_HP': ['Storage', 'all, high power-energy-ratio'],
+              'ElectricityGrid_all': ['Grid Expansion', 'all'],
+              'ElectricityGrid_on': ['Grid Expansion', 'onshore only'],
+              'ElectricityGrid_off': ['Grid Expansion', 'offshore only'],
+              'ElectricityGrid_noBorder': ['Grid Expansion', 'no Border'],
+              'Hydrogen_Baseline': ['Hydrogen', 'all'],
+              'Hydrogen_H1': ['Hydrogen', 'no storage'],
+              'Hydrogen_H2': ['Hydrogen', 'no hydrogen offshore'],
+              'Hydrogen_H3': ['Hydrogen', 'no hydrogen onshore'],
+              'Hydrogen_H4': ['Hydrogen', 'local use only'],
+              'All': ['All', 'All']
+             }
+
+def map_timestamp(timestamp, idx):
+    for key, value in scenarios.items():
+        if key in timestamp:
+            return value[idx]
+    return None  # or some default value if no match is found
+
+def add_prefix_to_keys(dictionary, prefix):
+    new_dict = {}
+    for key, value in dictionary.items():
+        new_key = prefix + key
+        new_dict[new_key] = value
+    return new_dict
+
+
+df = pd.read_excel(dir)
+
+df['Case'] = df['time_stamp'].apply(lambda x: map_timestamp(x, 0))
+df['Subcase'] = df['time_stamp'].apply(lambda x: map_timestamp(x, 1))
+
+# Normalization
+baseline_costs = df.loc[df['Case'] == 'Baseline', 'total_costs'].values[0]
+baseline_emissions = df.loc[df['Case'] == 'Baseline', 'net_emissions'].values[0] + h2_emissions
+df['normalized_costs'] = df['total_costs'] / baseline_costs
+df['normalized_emissions'] = ((df['net_emissions'] +  + h2_emissions) /
+                              baseline_emissions)
+df['delta_cost'] = df['total_costs'] - baseline_costs
+df['delta_emissions'] = df['net_emissions'] - baseline_emissions
+df['abatemente_cost'] = round(df['delta_cost'],0) / round(df['delta_emissions'],0)
+
+
+# IMPORTS AND CURTAILMENT
+max_re = pd.read_csv('C:/Users/6574114/PycharmProjects/PyHubProductive/mes_north_sea/clean_data/production_profiles_re/production_profiles_re.csv', index_col=0, header=[0,1])
+max_re = max_re.loc[:, (slice(None), 'total')].sum()
+max_re.reset_index(level=1, drop=True, inplace=True)
+
+# Get nodes and carriers
+with h5py.File(
+    df.loc[df['Case'] == 'Baseline', 'time_stamp'].values[0] + '/optimization_results.h5',
+    'r') as hdf_file:
+    nodes = extract_dataset_from_h5(hdf_file["topology/nodes"])
+    carriers = extract_dataset_from_h5(hdf_file["topology/carriers"])
+
+
+# Calculate Imports and Curtailment
+imports_dict = {}
+export_dict = {}
+curtailment_dict = {}
+generic_production_dict = {}
+tec_output_dict = {}
+demand_dict = {}
+for idx, row in df.iterrows():
+    case_path = df.loc[idx, "time_stamp"]
+
+    print(case_path)
+    # Read data
+    with h5py.File(case_path + '/optimization_results.h5', 'r') as hdf_file:
+        df_case = extract_datasets_from_h5group(hdf_file["operation/energy_balance"])
+    df_case = df_case.sum()
+
+    # Imports
+    imports_df = df_case.loc[:, 'electricity', 'import']
+    prefixed_dict = add_prefix_to_keys(imports_df.to_dict(), 'import_')
+    prefixed_dict['import_total'] = imports_df.sum()
+    imports_dict[case_path] = prefixed_dict
+
+    # Exports
+    export_df = df_case.loc[:, 'hydrogen', 'export']
+    prefixed_dict = add_prefix_to_keys(export_df.to_dict(), 'export_')
+    prefixed_dict['export_total'] = export_df.sum()
+    export_dict[case_path] = prefixed_dict
+
+    # Curtailment
+    generic_production_df = df_case.loc[:, 'electricity', 'generic_production']
+    curtailment_df = max_re - generic_production_df
+
+    prefixed_dict = add_prefix_to_keys(curtailment_df.to_dict(), 'curtailment_')
+    prefixed_dict['curtailment_total'] = curtailment_df.sum()
+    curtailment_dict[case_path] = prefixed_dict
+
+    prefixed_dict = add_prefix_to_keys(generic_production_df.to_dict(), 'generic_production_')
+    prefixed_dict['generic_production_total'] = generic_production_df.sum()
+    generic_production_dict[case_path] = prefixed_dict
+
+    # Demand
+    demand_df = df_case.loc[:, 'electricity', 'demand']
+    prefixed_dict = add_prefix_to_keys(demand_df.to_dict(), 'demand_')
+    prefixed_dict['demand_total'] = demand_df.sum()
+    demand_dict[case_path] = prefixed_dict
+
+    # Technology operation
+    with h5py.File(case_path + '/optimization_results.h5', 'r') as hdf_file:
+        df_case = extract_datasets_from_h5group(hdf_file["operation/technology_operation"])
+    df_case = df_case.sum()
+    tec_output = df_case.loc[:, :, 'electricity_output']
+    tec_output = tec_output.groupby(level=1).sum()
+    tec_output_dict[case_path] = tec_output.to_dict()
+
+
+# Merge all
+imports_df_all = pd.DataFrame.from_dict(imports_dict, orient='index')
+export_df_all = pd.DataFrame.from_dict(export_dict, orient='index')
+curtailment_all = pd.DataFrame.from_dict(curtailment_dict, orient='index')
+generic_production_all = pd.DataFrame.from_dict(generic_production_dict, orient='index')
+tec_output_all = pd.DataFrame.from_dict(tec_output_dict, orient='index')
+demand_all = pd.DataFrame.from_dict(demand_dict, orient='index')
+
+df = df.set_index('time_stamp')
+df_appended = pd.merge(df, curtailment_all, right_index=True, left_index=True)
+df_appended = pd.merge(df_appended, imports_df_all, right_index=True, left_index=True)
+df_appended = pd.merge(df_appended, export_df_all, right_index=True, left_index=True)
+df_appended = pd.merge(df_appended, generic_production_all, right_index=True, left_index=True)
+df_appended = pd.merge(df_appended, tec_output_all, right_index=True, left_index=True)
+df_appended = pd.merge(df_appended, demand_all, right_index=True, left_index=True)
+
+df_appended['h2_emissions'] = h2_emissions - df_appended['export_total']* 0.108
+df_appended['total_emissions'] = (df_appended['net_emissions'] +
+                                  df_appended['h2_emissions'])
+df_appended['electricity_emissions'] = (df_appended['total_emissions'] -
+                                        df_appended['h2_emissions'])
+
+df_appended = df_appended.set_index(['Case', 'Subcase'])
+df_appended.to_excel(dir_processed, merge_cells=False)
+# df = df.set_index(['Case', 'Subcase'])
+# df.to_excel(dir_processed, merge_cells=False)
+
+
